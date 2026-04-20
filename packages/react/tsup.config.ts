58edc070776f20af65183c52fbfe983ea05da1cb
bgskinner3 <bgskinner3@gmail.com>
2026-04-20 18:48:59 -0400

    wip

---
+++ packages/react/tsup.config.ts
@@ -0,0 +1,1 @@
+// packages/react/tsup.config.ts
---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  endOfLine: 'lf',
+  trailingComma: 'all',
+};
